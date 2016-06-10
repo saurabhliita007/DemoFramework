55de3cf9096a61735d50b5e8599c55c66d28bec1
saurabhliita007 <saurabhliita007@gmail.com>
2016-06-10 13:16:42 +0530

    Added all the files

---
+++ DemoFramework/Utility.h
@@ -0,0 +1,15 @@
+//
+//  Utility.h
+//  DemoFramework
+//
+//  Created by VS-Saurabh-MacbookPro on 10/06/16.
+//  Copyright © 2016 VS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utility : NSObject
+
++ (BOOL) validatePhoneNumber:(NSString *)phoneNumber;
+
+@end
